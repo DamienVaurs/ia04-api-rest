b4a1081eab62e968995766107cd60e958de6b815
milairhu <hugo.milair@gmail.com>
2023-10-10 10:40:31 +0200

    /vote : vérifie les alternatives fournies

--- restagent/restserveragent/vote.go
+++ restagent/restserveragent/vote.go
@@ -5,9 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sort"
 	"time"
 
 	"gitlab.utc.fr/milairhu/ia04-api-rest/restagent"
+	"gitlab.utc.fr/milairhu/ia04-api-rest/restagent/comsoc"
 )
 
 // Fonctions qui traitent l'appel à l'API REST pour voter:
@@ -23,6 +25,24 @@ func (*RestServerAgent) decodeVoteRequest(r *http.Request) (req restagent.Reques
 
 	}
 	return
+}
+
+func checkVoteAlts(vote []comsoc.Alternative, expected int) bool {
+	//vérifie que le vote correspond aux alternatives proposées par le ballot
+
+	//Rappel : les alternatives attendues sont entre 1 et Ballot.Alts inclus
+	if len(vote) != expected {
+		return false
+	}
+	list := make([]comsoc.Alternative, expected)
+	copy(list, vote)
+	sort.Slice(list, func(i, j int) bool { return list[i] < list[j] })
+	for i := 0; i < expected-1; i++ {
+		if list[i] != comsoc.Alternative(i+1) {
+			return false
+		}
+	}
+	return true
 }
 
 func (rsa *RestServerAgent) doVote(w http.ResponseWriter, r *http.Request) {
@@ -80,6 +100,14 @@ func (rsa *RestServerAgent) doVote(w http.ResponseWriter, r *http.Request) {
 	if rsa.ballotsList[req.BallotId].Deadline.Before(time.Now()) {
 		w.WriteHeader(http.StatusBadRequest)
 		msg := fmt.Sprintf("error /result : ballot %s is already fnished", req.BallotId)
+		w.Write([]byte(msg))
+		return
+	}
+
+	//Vérifie que les alteratives fournies pour le vote sont correctes
+	if !checkVoteAlts(req.Prefs, rsa.ballotsList[req.BallotId].Alts) {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := fmt.Sprintf("error /vote : alternatives provided for ballot %s are not correct", req.BallotId)
 		w.Write([]byte(msg))
 		return
 	}
